1412a1e6c471e2d6e78556ac4303be534412d544
Jtawfik04 <94028535+Jtawfik04@users.noreply.github.com>
2022-07-17 13:43:24 -0400

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import CopticCalendar from './Components/Calendar.js';
 
 function App() {
   return (
--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import React from 'react';
-import 'react-calendar/dist/Calendar.css';
+//import 'react-calendar/dist/Calendar.css';
 
-function Calendar = () => {
-const [date, setDate] = useState(new Date());
+function CopticCalendar() {
+  const [date, setDate] = useState(new Date());
   const [holiday, setHoliday] = useState('');
 
   return (
+    <>
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
       </div>
@@ -15,9 +16,9 @@ const [date, setDate] = useState(new Date());
         <span className='bold'>Selected Date:</span>{' '}
         {date.toDateString()}
       </p>
+      </>
   );
 
 }
 
-export default Calendar;
-
+export default CopticCalendar;
