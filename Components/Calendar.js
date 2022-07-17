959b2ef2c01cc7c4d8ea0a4624988bbd421c85f7
Jtawfik04 <94028535+Jtawfik04@users.noreply.github.com>
2022-07-17 13:31:05 -0400

    Create Calendar.js

---
+++ Components/Calendar.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import React from 'react';
+import 'react-calendar/dist/Calendar.css';
+
+function Calendar = () => {
+const [date, setDate] = useState(new Date());
+  const [holiday, setHoliday] = useState('');
+
+  return (
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+  );
+
+}
+
+export default Calendar;
+
